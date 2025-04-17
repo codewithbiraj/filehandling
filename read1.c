54d0900235b4cba9da328d8cff5079d03283b98b
codewithbiraj <joshibiraj113@gmail.com>
2025-04-17 16:02:21 +0545

    Add files via upload

---
+++ read1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+FILE *fptr;
+fptr=fopen("hello.txt","w");
+if(fptr==NULL){
+printf("the file doestn't exist");
+
+}else{
+
+fclose(fptr);
+}
+
+
+
+
+
+return 0;
+}
